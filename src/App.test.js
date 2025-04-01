d4a0fd5a444d1eb4c8c8b70a4b6bbbe0bffbca73
Samuel <106888031+Alpha72142@users.noreply.github.com>
2025-04-01 19:08:27 +0530

    changed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
 import FirstHeroSection from "./component/FirstHeroSection";
 
 describe("testing", () => {
